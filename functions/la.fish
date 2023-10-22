ee21f291cd1672d7ef35dd8b8d387db4d067f5df
lumynou5 <lumynou5.tw@gmail.com>
2023-10-22 09:57:24 +0800

    feat!: la passes --long now

--- functions/la.fish
+++ functions/la.fish
@@ -1,3 +1,3 @@
-function la --wraps 'eza --icons --all' --description 'List directory contents including dot files'
-    command eza --icons --all $argv
+function la --wraps 'eza --icons --long --all' --description 'List directory contents including dot files'
+    command eza --icons --long --all $argv
 end
